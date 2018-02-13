cf57724e9397b1d7cbd5e299a7dbee601e593ca5
root <root@git.lunixcloud.com>
2018-02-13 08:53:20 +0100

    Debut codeigniter

--- old/recipe.php
+++ old/recipe.php
@@ -44,22 +44,35 @@
 
 
             <!-- CONTENT BEGIN -->
-            
-        <section class="section">
-          <div class="container">
-            <div class="has-text-centered has-centered">
-              <h1>Space Cake </h1>
-                  <img src="img/space_cake.jpg" width="256">
+            <section class="recette">
+
+              <div class="columns">
+                <div class="column is-one-third"></div>
+                <div class="column has-text-centered">
+                  <h1 class="title">Recette : Le Space Cake</h1>
+                </div>
+                <div class="column"></div>
               </div>
-            </div>
-            </div>
-          </div>
-        </section>
-            
+
+              <div class="columns">
+                  <div class="column is-one-third"></div>
+                  <div class="column has-text-centered">
+                      <figure class="image is-4by3">
+                          <img src="img/space_cake.jpg" alt="Placeholder image">
+                        </figure>
+                  </div>
+                  <div class="column"></div>
+              </div>
+
+              <div class="columns">
+                  <div class="column is-half">is-half</div>
+                  <div class="column">Auto</div>
+                  <div class="column">Auto</div>
+              </div>
 
 
 
-
+          </section>
 
             <!-- CONTENT END -->
 
